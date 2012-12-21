dab2cbc9fadcfc99a479ad509b7b4f2fc7a23c1f
Dan Zajdband <dan.zajdband@gmail.com>
2012-12-20 21:30:39 -0300

    added markup to project_full view description

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 module.exports = function(app) {
 
   app.locals.moment = require('moment');
+  app.locals.md = require('markdown').markdown.toHTML;
 
   require('./site')(app);
   require('./api')(app);
