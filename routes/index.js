7c0c83056d2dc6bb87138d948f2892a075a42fc0
Damián Silvani <munshkr@gmail.com>
2013-03-16 16:23:52 -0300

    Show Edit/Remove links always if user is admin

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ module.exports = function(app) {
   app.locals.moment = require('moment');
   app.locals.md = require('markdown').markdown.toHTML;
 
+  app.locals.isLeader = function(user, project) {
+    return (user.is_admin || user.id == project.leader);
+  };
+
   require('./site')(app);
   require('./api')(app);
   require('./admin')(app);
