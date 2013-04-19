f293734739ff5b58fcf56f56261cd7282f8bd333
Dan Zajdband <dan.zajdband@gmail.com>
2013-04-19 13:19:03 -0300

    added visual subdomain validation

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,10 +1,16 @@
 
 ;(function(){
 
-$('#create-dashbord').submit(function(){
-  if(!/\w{5,10}/.test($(this).find('input').val())) {
-    $(this).find('p.error').text('Please provide 5 or more letters').show();
-    return false;
+var $submitBtn = $('input[type=submit]');
+
+$('#domain').keyup(function(){
+  var name = $(this).val();
+  if(/^\w{5,10}$/.test(name)) {
+    $(this).parent().addClass('success').removeClass('error');
+    $submitBtn.removeClass('disabled');
+  } else {
+    $(this).parent().addClass('error').removeClass('success');
+    $submitBtn.addClass('disabled');
   }
 });
 
