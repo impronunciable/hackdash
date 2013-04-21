4177eedfa22dde8eda7f82ed02c7143aec0d1412
Dan Zajdband <dan.zajdband@gmail.com>
2013-04-21 19:15:13 -0300

    fixed issue 22

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || app.get('config').port);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.use(express.favicon(__dirname + '/public/favicon.ico'));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.limit('3.5mb'));
