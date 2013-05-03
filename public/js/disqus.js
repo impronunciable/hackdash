24c8f01aad3d49c84f11b21f9f8c8a9d698a4ccc
Matias Saguir <mativs@gmail.com>
2013-05-03 15:06:39 -0300

    Disqus working

---
+++ public/js/disqus.js
@@ -0,0 +1,12 @@
+/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+  var disqus_shortname = 'bahackatonhackdash'; // required: replace example with your forum shortname
+  var disqus_config = function () { 
+    this.language = "es_ES";
+  };
+
+  /* * * DON'T EDIT BELOW THIS LINE * * */
+  (function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  })();     
