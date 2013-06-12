d274079e2b30f62ae97e73c677f80780da0e0e31
Dan Zajdband <dan.zajdband@gmail.com>
2013-06-12 10:33:23 -0300

    fixed domain validation

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -5,7 +5,7 @@ var $submitBtn = $('input[type=submit]');
 
 $('#domain').keyup(function(){
   var name = $(this).val();
-  if(/^\w{5,10}$/.test(name)) {
+  if(/^[a-z0-9]{5,10}$/.test(name)) {
     $(this).parent().addClass('success').removeClass('error');
     $submitBtn.removeClass('disabled');
   } else {
