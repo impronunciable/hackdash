fb3fe9fc47381befa347658017709ec8a12589ba
Dan Zajdband <dan.zajdband@gmail.com>
2013-06-20 12:40:46 -0300

    disqus in english

--- public/js/disqus.js
+++ public/js/disqus.js
@@ -4,7 +4,7 @@ var thisScriptTag = scripts[ scripts.length - 1 ];
 var disqus_shortname = thisScriptTag.getAttribute('disqus_shortname') ; // required: replace example with your forum shortname
 if ( disqus_shortname != '' ) {
   var disqus_config = function () { 
-    this.language = "es_ES";
+    this.language = "en_EN";
   };
 
   /* * * DON'T EDIT BELOW THIS LINE * * */
