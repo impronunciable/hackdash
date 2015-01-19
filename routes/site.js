34d2bca2dfec80e4272b866dd4c6ec8f1bc99881
Pablo Novas <pjnovas@gmail.com>
2015-01-19 17:49:29 -0300

    adde dynamic metas from server-side. #112

--- routes/site.js
+++ routes/site.js
@@ -5,13 +5,14 @@ var passport = require('passport')
 
 var User = mongoose.model('User')
   , Project = mongoose.model('Project')
-  , Dashboard = mongoose.model('Dashboard');
+  , Dashboard = mongoose.model('Dashboard')
+  , Collection = mongoose.model('Collection');
 
 module.exports = function(app) {
 
   var liveStack = [
     isLive(app),
-    loadUser, 
+    loadUser,
     loadProviders,
     dashExists,
     setViewVar('statuses', app.get('statuses')),
@@ -24,7 +25,7 @@ module.exports = function(app) {
   var appHost = app.get('config').host + (appPort && appPort !== 80 ? ':' + appPort : '');
 
   var hackdashFullStack = [
-    loadUser, 
+    loadUser,
     loadProviders,
     setSubdomain,
     checkProfile,
@@ -32,6 +33,7 @@ module.exports = function(app) {
     setViewVar('version', app.get('clientVersion')),
     setViewVar('statuses', app.get('statuses')),
     setViewVar('disqus_shortname', config.disqus_shortname),
+    checkMetas(),
     render('hackdashApp')
   ];
 
@@ -42,7 +44,7 @@ module.exports = function(app) {
 
   // home page if no subdomain
   // dashboard if subdomain
-  app.get('/', hackdashFullStack);
+  app.get('/', setDashboardMetas, hackdashFullStack);
 
   // Search all projects if no subdomain
   // Search projects inside dashboard if subdomain
@@ -53,7 +55,7 @@ module.exports = function(app) {
   app.get('/projects/create', hackdashFullStack);
 
   // Project View - Always Read Only
-  app.get('/projects/:pid', hackdashFullStack);
+  app.get('/projects/:pid', setProjectMetas, hackdashFullStack);
 
   // Project Edit Form - ONLY with a domain - redirects if not
   app.get('/projects/:pid/edit', hackdashFullStack);
@@ -63,7 +65,7 @@ module.exports = function(app) {
   app.get('/dashboards', hackdashFullStack);
 
   // Dashboards by domain
-  app.get('/dashboards/:dashboard', hackdashFullStack);
+  app.get('/dashboards/:dashboard', setDashboardMetas, hackdashFullStack);
 
   // Collections search - ONLY without subdomain - redirects if any
   // ?q=[query]
@@ -71,26 +73,26 @@ module.exports = function(app) {
 
   // Collection View - ONLY without subdomain - redirects if any
   // Shows a list of dashboards - NO SEARCH
-  app.get('/collections/:cid', hackdashFullStack);
+  app.get('/collections/:cid', setCollectionMetas, hackdashFullStack);
 
   // User Profile's View
   app.get('/users/profile', hackdashProfileStack);
 
   // Users Profile's card and entities
-  app.get('/users/:user_id', hackdashFullStack);
+  app.get('/users/:user_id', setUserMetas, hackdashFullStack);
 
   // Live view of a Dashboard - ONLY with a subomain
   app.get('/live', liveStack);
 
   // Login view
   app.get('/login', hackdashFullStack);
-  
+
   // Logout
   app.get('/logout', logout, redirect('/'));
 
   //keep previous Projects link working
-  app.get('/p/:pid', function(req, res){ 
-    res.redirect('/projects/' + req.params.pid);
+  app.get('/p/:pid', function(req, res){
+    res.redirect(301, '/projects/' + req.params.pid);
   });
 
 };
@@ -118,7 +120,7 @@ var setSubdomain = function(req, res, next){
   res.locals.subdomain = null;
 
   if (req.subdomains.length){
-    res.locals.subdomain = req.subdomains[0]; 
+    res.locals.subdomain = req.subdomains[0];
   }
 
   next();
@@ -140,6 +142,31 @@ var isLive = function(app) {
       res.send(404);
     }
   }
+};
+
+var checkMetas = function(){
+  return function(req, res, next) {
+
+    if (!res.locals.meta){
+      res.locals.meta = {};
+    }
+
+    if (res.locals.meta.title){
+      res.locals.meta.title += " - ";
+    }
+    else {
+      res.locals.meta.title = "";
+    }
+
+    if (!res.locals.meta.image){
+      res.locals.meta.image = "/images/logohack.png";
+    }
+
+    res.locals.meta.title += "HackDash";
+    res.locals.meta.image = "//hackdash.org" + res.locals.meta.image;
+
+    next();
+  };
 };
 
 /*
@@ -170,7 +197,7 @@ var setViewVar = function(key, value) {
     res.locals[key] = value;
     next();
   };
-};  
+};
 
 /*
  * Load app providers
@@ -196,4 +223,87 @@ var dashExists = function(req, res, next) {
     res.locals.dashboard = dash;
     next();
   });
+};
+
+/*
+ * Set Metas
+ */
+
+var setProjectMetas = function(req, res, next){
+  Project.findById(req.params.pid, function(err, project) {
+      if (err || !project) return next();
+      // TODO: NotFound
+      res.locals.meta = {
+        title: project.title,
+        description: project.description,
+        image: project.cover
+      };
+
+      next();
+    });
+};
+
+var setDashboardMetas = function(req, res, next){
+  var domain;
+
+  if (req.subdomains.length > 0) {
+    domain = req.subdomains[0];
+  }
+  else if (req.params.dashboard) {
+    domain = req.params.dashboard;
+  }
+  else {
+    // Home Page
+    res.locals.meta = {
+      description: "Ideas for a hackathon. Upload your project. Add colaborators. Inform status. Share your app."
+    };
+
+    return next();
+  }
+
+  if (!domain){
+    return next();
+  }
+
+  Dashboard
+    .findOne({ domain: domain }, function(err, dashboard) {
+      if(err || !dashboard) return res.send(500);
+      // TODO: NotFound
+      res.locals.meta = {
+        title: dashboard.title,
+        description: dashboard.description
+      };
+
+      next();
+    });
+
+};
+
+var setCollectionMetas = function(req, res, next){
+  Collection.findById(req.params.cid, function(err, collection) {
+      if (err || !collection) return next();
+      // TODO: NotFound
+
+      res.locals.meta = {
+        title: collection.title,
+        description: collection.description
+      };
+
+      next();
+  });
+};
+
+var setUserMetas = function(req, res, next){
+  User
+    .findById(req.params.user_id, function(err, user){
+      if(err || !user) return next();
+      // TODO: NotFound
+
+      res.locals.meta = {
+        title: user.name,
+        description: user.bio
+      };
+
+      next();
+    });
 };
