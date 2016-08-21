f3c569067e346d167f81c567b798c828254b4c29
Julián Rodriguez Orihuela <julianrod@gmail.com>
2016-08-21 02:43:43 -0300

    agrego 1ra localización
    agregué función de localización y traducción al español

--- client/app/locale/es.js
+++ client/app/locale/es.js
@@ -1,4 +1,4 @@
-/*eslint-disable */
+﻿/*eslint-disable */
 module.exports = {
   "code": "es-ES",
   "time_format": "HH:mm",
@@ -7,6 +7,180 @@ module.exports = {
   "datetime_format": "DD\/MM\/YYYY HH:mm",
   "colloquial_date_format": "ddd Do MMMM YYYY",
 
-  "dashboards": "Tableros",
-  "create_now": "crear ahora"
+
+  /* Home Directory */
+
+/* home.hbs */
+
+  "Dashboards": "Tableros",
+  "create now": "crear ahora",
+  "ERROR":"ERROR",
+  "Ideas for a": "Ideas para una",
+  "hackathon":"hackatón",
+  "Collections":"Colecciones",
+  "Projects":"Proyectos",
+  "People":"Personas",
+  "team":"equipo",
+  "partners":"socios",
+  "The HackDash was born":"HackDash nació por accidente y por necesidad. Estábamos buscando una plataforma para hacer seguimiento de ideas durante los hackatones en la línea de <a href=\"http://mediaparty.info/\" data-bypass=\"true\" target=\"__blank\">Hacks/Hackers Media Party</a> organizado por <a href=\"https://twitter.com/HacksHackersBA\" data-bypass=\"true\" target=\"__blank\">@HacksHackersBA</a> en la que hackers y periodistas comparten ideas. Corrimos la voz de nuestra necesidad por Twitter y ese fue el contexto en el que nació HackDash. <a href=\"https://twitter.com/blejman\" data-bypass=\"true\" target=\"__blank\">@blejman</a> tuvo una idea y a <a href=\"https://twitter.com/dzajdband\" data-bypass=\"true\" target=\"__blank\">@dzajdband</a> le interesó implementar esa idea. Así que empezamos a crear la aplicación esperando llegar al Buenos Aires Media Party con algo que no fuera horrible. El día del hackatón de Media Party llegó acompañado de una grata sorpresa: No solamente HackDash le gustó a la gente, sino que también algunos programadores agregaron la mejora de HackDash como su proyecto de Hackatón. Después del Media Party nos dimos cuenta de que esta pequeña aplicación estaba cubriendo una necesidad real. Tres años después, el tablero se está convirtiendo en un estándar para hacer seguimiento de ideas innovadores alrededor del mundo.<p><a class=\"up-button\">Creá tu propio tablero</a>, sé parte de una comunidad global.</p>,
+  
+/* collection.hbs */  
+
+/* counts.hbs */
+
+"dashboards":"tableros",
+"projects":"proyectos",
+"registered users":"usuarios registrados",
+"collections":"colectciones",
+"released projects":"proyectos lanzados",
+
+/* footer.hbs */
+
+"up":"subir",
+
+/* search.hbs */
+
+"Inform Progress to community.":"Informa el progreso a la comunidad.",
+"Upload your project to the platform.":"Sube tu proyecto a la plataforma."
+"find it":"encuéntralo",
+"Add Collaborators to your projects.":"Agrega colaboradores a tu proyecto.",
+"Share your app to the world.":"Comparte tu aplicación con el mundo.",
+
+/* tabContent.hbs */
+
+"HEAD":"ENCABEZADO",
+
+/* Collections directory */
+
+/* list.hbs */
+
+"My Collections: adding":"Mis colecciones: agregando",
+
+/* listItem.hbs */
+
+"View":"Ver",
+
+/* Dashboard directory */
+
+/* addAdmin.hbs */
+
+"Warning! you will NOT":"¡Atención! ¡NO podrás eliminar este tablero si agregas un administrador!",
+"type name or username":"escribe nombre o nombre de usuario",
+"cancel":"cancelar",
+
+/* index.hbs */
+
+"Open dashboard website":"Abrir el sitio del tablero",
+"Share this Dashboard":"Compartir este tablero",
+"Create Project":"Crear proyecto",
+
+/* share.hbs */
+
+"embed this dashboard":"insertar este tablero",
+"Slider":"Slider",
+"ANY STATUS":"CUALQUIER ESTADO",
+"Add this dashboard to your website by coping this code below":"Agrega este tablero a tu sitio web copiando el código de abajo",
+"By Name":"Por nombre",
+"By Date":"Por fecha",
+"Showcase":"Galería",
+"Share Link":"Enlace para compartir",
+"Preview":"Previsualización",
+"embedded_code":"El código de inserción mostrará exactamente lo que está abajo",
+
+
+/* users.hbs */
+
+"Add admins":"Agregar administradores",
+
+
+/* Footer directory */
+
+/* footer.hbs */
+
+"Export .CSV File":"Exportar archivo .CSV",
+"Open":"Abrir",
+"Close":"Cerrar",
+"Dashboard Status":"Estado del tablero",
+"off":"apagar",
+"Edit Showcase":"Editar garlería",
+
+/* Header directory */
+
+/* header.hbs */
+
+"Log out":"Salir",
+"Log in":"Ingresar",
+
+/* Profile directory */
+
+/* card.hbs */
+
+"[ Log in to reveal e-mail ]":"[ Ingresa para revelar el correo ]",
+
+/* cardEdit.hbs */
+
+"Edit Your Profile":"Edita tu perfil",
+"all fields required":"todos los campos obligatorios",
+"email only visible for logged in users":"correo sólo visible para usuarios ingresados",
+"about_you":"Algo sobre tí",
+"saving...":"guardando...",
+"Save profile":"Guardar perfil",
+"Profile saved, going back to business ...":"Perfil guardado, volviendo...",
+
+/* listItem.hbs */
+
+"Remove":"Eliminar",
+
+/* profile.hbs */
+
+"Contributions":"Colaboraciones",
+"following":"Siguiendo",
+
+/* Project directory */
+
+/* card.hbs */
+
+"Join":"Unirse",
+"Follow":"Seguir",
+"Leave":"Abandonar",
+"Unfollow":"Dejar de seguir",
+"Demo":"Demo",
+
+/* edit.hbs */
+
+"Project Title":"Título del proyecto",
+"Import Project":"Importar proyecto",
+"GitHub":"GitHub",
+"LOADING":"CARGANDO",
+"import":"importar",
+"Tags":"Etiquetas (valores separados por comas)",
+"Project URL Demo":"URL de demo del proyecto",
+"Save":"Guardar",
+"Cancel":"Cancelar",
+
+/* full.hbs */
+
+"leaving...":"abandondando...",
+"joining...":"uniéndose...",
+"unfollowing...":"dejando de seguir...",
+"following...":"siguiendo...",
+"Share this Project":"Comparte este proyecto",
+"Managed by":"Administrado por",
+"Contributors":"Colaboradores",
+"Edit":"Editar",
+
+
+/* share.hbs */
+
+"embed this project":"inserta este proyecto",
+"Add this project to your website by coping this code below":"Agrega este proyecto a tu sitio copiando el código de abajo",
+
+
+/* templates directory */
+
+"Access with":"Acceder con",
+
+"embed/insert":"insertar" 
+  
+  
 };
