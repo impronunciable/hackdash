d9cc14b79347d27e7b92eacc57cd72d71db4e39b
Seb35 <seb35@seb35.fr>
2018-03-09 00:27:43 +0100

    Translation to French

---
+++ client/app/locale/fr.js
@@ -0,0 +1,274 @@
+/*eslint-disable */
+module.exports = {
+  "code": "fr-FR",
+  "time_format": "HH:mm",
+  "date_locale": "fr",
+  "date_format": "DD\/MM\/YYYY",
+  "datetime_format": "DD\/MM\/YYYY HH:mm",
+  "colloquial_date_format": "ddd Do MMMM YYYY",
+
+
+/* Home Directory */
+
+/* home.hbs */
+
+  "Dashboards": "Tableaux de bord",
+  "create now": "créer, maintenant",
+  "ERROR":"ERREUR",
+  "Ideas for a": "Idées pour un",
+  "hackathon":"hackathon",
+  "Collections":"Collections",
+  "Projects":"Projets",
+  "People":"Personnes",
+  "team":"équipe",
+  "partners":"partenaires",
+  "The HackDash was born":"Le HackDash est né par accident et par besoin. Nous recherchions une plate-forme pour suivre les idées lors des hackathons tel que le <a href=\"http://mediaparty.info/\" data-bypass=\"true\" target=\"__blank\">Hacks/Hackers Media Party</a> organizé par <a href=\"https://twitter.com/HacksHackersBA\" data-bypass=\"true\" target=\"__blank\">@HacksHackersBA</a> où des hackers et des journalists partagent des idéees. Nous avons diffusé le besoin via Twitter : c’est dans ce contexte que le HackDash est né. <a href=\"https://twitter.com/blejman\" data-bypass=\"true\" target=\"__blank\">@blejman</a> a eu l’idée et <a href=\"https://twitter.com/dzajdband\" data-bypass=\"true\" target=\"__blank\">@dzajdband</a> fut intéressé pour implémenter l’idée. Nous commençâmes ainsi à contruire l’application en espérant que nous pourrions arriver au Buenos Aires Media Party avec quelque chose qui ne soit pas râté. Le jour du Media Party Hackathon arriva, suivi par une formidable surprise. Non seuleemnt les gens appréciaient l’implémentation du HackDash mais une poignée de codeurs ont ajouté l’amélioration du HackDash comme projet du Hackathon. Après le Media Party nous avons réalisé que cette petite application remplissait un réel besoin. Trois ans plus tard, le tableau de bord est devenu un standard pour suivre les idées innovantes dans le monde entier.<p><a class=\"up-button\">Créez votre propre tableau de bord</a>, prenez part à la communauté mondiale.</p>",
+
+/* collection.hbs */
+
+/* counts.hbs */
+
+"dashboards":"tableaux de bord",
+"projects":"projets",
+"registered users":"utilisateurs inscrits",
+"collections":"collections",
+"released projects":"projets publiés",
+
+/* footer.hbs */
+
+"up":"haut",
+
+/* search.hbs */
+
+"Inform Progress to community.":"Informez la communauté de l’avancement.",
+"Upload your project to the platform.":"Versez vos projets sur la plate-forme.",
+"find it":"le trouver",
+"Add Collaborators to your projects.":"Ajoutez des collaborateurs à vos projets.",
+"Share your app to the world.":"Partagez votre application avec le monde.",
+
+/* tabContent.hbs */
+
+"HEAD":"HEAD",
+
+/* Collections directory */
+
+/* list.hbs */
+
+"My Collections: adding":"Mes collections : ajouter",
+
+/* listItem.hbs */
+
+"View":"Voir",
+
+/* Dashboard directory */
+
+/* addAdmin.hbs */
+
+"Warning! you will NOT":"Attention ! vous ne pourrez PAS supprimer ce tableau de bord si vous ajoutez un admin !",
+"type name or username":"entrez votre nom ou pseudonyme",
+"cancel":"annuler",
+
+/* index.hbs */
+
+"Open dashboard website":"Ouvrir le site web du tableau de bord",
+"Share this Dashboard":"Partager ce tableau de bord",
+"Create Project":"Créer un projet",
+
+/* share.hbs */
+
+"embed this dashboard":"embarquer ce tableau de bord",
+"Slider":"Slider",
+"ANY STATUS":"TOUT STATUT",
+"Add this dashboard to your website by coping this code below":"Ajoutez ce tableau de bord à votre site web en copiant le code ci-dessous",
+"By Name":"Par nom",
+"By Date":"Par date",
+"Showcase":"Gallerie",
+"Share Link":"Partager le lien",
+"Preview":"Prévisualiser",
+"embedded_code":"Le code embarqué affichera exactement ce qui suit",
+
+
+/* users.hbs */
+
+"Add admins":"Ajouter des admins",
+
+
+/* Footer directory */
+
+/* footer.hbs */
+
+"Export .CSV File":"Exporter le fichier .CSV",
+"Open":"Ouvrir",
+"Close":"Fermer",
+"Dashboard Status":"Statut du tableau de bord",
+"off":"off",
+
+/* Header directory */
+
+/* header.hbs */
+
+"Log out":"Se déconnecter",
+"Log in":"Se connecter",
+
+/* Profile directory */
+
+/* card.hbs */
+
+"[ Log in to reveal e-mail ]":"[ Connectez-vous pour révéler le courriel ]",
+
+/* cardEdit.hbs */
+
+"Edit Your Profile":"Modifiez votre profil",
+"all fields required":"tous les champs sont requis",
+"email only visible for logged in users":"le courriel est visible seulement pour les utilisateurs inscrits",
+"about_you":"Quelques mots à propos de vous",
+"saving...":"enregistrement...",
+"Save profile":"Enregistrer le profil",
+"Profile saved, going back to business ...":"Profil enregistré, retour aux affaires…",
+
+/* listItem.hbs */
+
+"Remove":"Retirer",
+
+/* profile.hbs */
+
+"Contributions":"Contributions",
+"following":"Suivi",
+
+/* Project directory */
+
+/* card.hbs */
+
+"Join":"Rejoindre",
+"Follow":"Suivre",
+"Leave":"Quitter",
+"Unfollow":"Ne plus suivre",
+"Demo":"Démo",
+
+/* edit.hbs */
+
+"Project Title":"Titre du projet",
+"Import Project":"Importer le projet",
+"GitHub":"GitHub",
+"LOADING":"CHARGEMENT",
+"import":"importer",
+"Tags":"Tags ( valeurs séparées par des virgules )",
+"Project URL Demo":"URL de démo du projet",
+"Save":"Enregistrer",
+"Cancel":"Annuler",
+
+/* full.hbs */
+
+"leaving...":"quitter…",
+"joining...":"rejoindre…",
+"unfollowing...":"fin du suivi…",
+"following...":"suivi…",
+"Share this Project":"Partager ce projet",
+"Managed by":"Géré par ",
+"Contributors":"Contributeurs",
+"Edit":"Modifier",
+
+
+/* share.hbs */
+
+"embed this project":"embarquer ce projet",
+"Add this project to your website by coping this code below":"Ajoutez ce projet sur votre site web en copiant le code ci-dessous",
+
+
+/* templates directory */
+
+"Access with":"Accès avec",
+"embed/insert":"embarqué/insérer",
+
+
+/* ----------------------- js files ------------------------ */
+
+/* Sharer.js */
+
+"Hacking at":"Je hacke à",
+
+/* Collection directory */
+
+/* Collection.js */
+
+"Collection of Hackathons Title":"Collection de titres de hackathons",
+"brief description of this collection of hackathons":"brève description de cette collection de hackathons",
+
+/* List.js */
+
+" has been added to ":" a été ajouté à ",
+" has been removed from ":" a été retiré de ",
+
+/* Dashboard directory */
+
+/* Dashboard.js */
+
+"Hackathon Title":"Titre du hackathon",
+"brief description of this hackathon":"brève description de ce hackathon",
+"url to hackathon site":"URL du site du hackathon",
+
+/* Share.js */
+
+"Description":"Description",
+"Hackdash Logo":"Logo du Hackdash",
+"Progress":"Progression",
+"Action Bar":"Barre d’action",
+
+/* Footer directory */
+
+/* index.js */
+
+"This Dashboard is open: click to close":"Ce tableau de bord est ouvert : cliquez pour le fermer",
+"This Dashboard is closed: click to reopen":"Ce tableau de bord est fermé : cliquez pour ré-ouvrir",
+"turned_off":"fermé",
+"Edit Showcase":"Modifier la gallerie",
+"Save Showcase":"Enregistrer la gallerie",
+
+/* Header directory */
+
+/* index.js */
+
+
+"Enter your keywords":"Entrez vos mots-clés",
+
+/* Home directory */
+/* index.js */
+
+"5 to 10 chars, no spaces or special":"5 à 10 caractères, pas d’espace ou de caractère spécial",
+"Sorry, that one is in use. Try another one.":"Désolé, celui-ci est utilisé. Essayez un autre.",
+
+/* Profile directory */
+/* CardEdit.js */
+
+"Name is required":"Un nom est requis",
+"Email is required":"Un courriel est requis",
+"Invalid Email":"Courriel invalide",
+
+/* ListItem.js */
+
+"Only the Owner can remove this Dashboard.":"Seul le propriétaire peut retirer ce tableau de bord.",
+"Only Dashboards with ONE admin can be removed.":"Seuls les tableaux de bord avec UN admin peuvent être retirés.",
+"Only Dashboards without Projects can be removed.":"Seuls les tableaux de bord sans projet peuvent être retirés.",
+"This action will remove Dashboard ":"Cette action va retirer le tableau de bord ",
+". Are you sure?":". Êtes-vous sûr ?",
+"cannot_remove_dashboard": "Impossible de retirer le tableau de bord {1}",
+
+/* Projects directory */
+/* Edit.js */
+
+"Title is required":"Un titre est requis",
+"Description is required":"Une description est requise",
+"Drop Image Here":"Glissez l’image ici",
+"File is too big, 500 Kb is the max":"Le fichier est trop gros, le maximum est 500 Kio",
+"Only jpg, png and gif are allowed":"Seulement les jpg, png ou git sont autorisés",
+
+/* Full.js */
+
+"This project is going to be deleted. Are you sure?":"Ce projet va être supprimé. Êtes-vous sûr ?",
+
+/* Share.js */
+
+"Picture":"Image",
+"Title":"Titre",
+
+};
