7cd8a5c046209e88878bf54b55b862e8507fccc4
JD Bothma <jbothma@gmail.com>
2021-05-27 22:13:49 +0200

    clean up .env file

--- config/config.js
+++ config/config.js
@@ -1,31 +1,32 @@
-require('dotenv').config()
+require('dotenv').config();
+
 module.exports = {
-    "db": {
-      "name": process.env.DB_NAME || "hackdash",
-      "host": process.env.DB_HOST || "localhost",
-      "url": process.env.DB_URL || "mongodb://db:27017"
-    },
-    "host": process.env.HOST || "local.host",
-    "port": parseInt(process.env.PORT) || 3000,
-    "session": process.env.SESSION || "wefewfef",
-    "discourseUrl": process.env.DISCOURSE_URL || null,
-    "disqus_shortname": process.env.DISQUS_SHORTNAME || null,
-    "title": process.env.TITLE || "hackdash",
-    "live": process.env.LIVE || true,
-    "mailer": process.env.MAILER || null,
-    "prerender": {
-      "enabled": process.env.PREPENDER_ENABLE || false,
-      "db": process.env.PREPENDER_DB || "mongodb://localhost/prerender"
-    },
-    "team": [
-      "516d1997b2951b02280000e1",
-      "516d1997b2951b02280000e2",
-      "516d1997b2951b02280000e3",
-      "516d1997b2951b02280000e4",
-      "516d1997b2951b02280000e5",
-      "516d1997b2951b02280000e6"
-    ],
-    "maxQueryLimit": process.env.MAX_QUERY_LIMIT || 30,
-    "googleAnalytics": process.env.GOOGLE_ANALYTICS || "UA-XXXXXXXX-X",
-    "facebookAppId": process.env.FCAEBOOK_APP_ID || "YYYYYYYYYYYY"
-}
+  "db": {
+    "name": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "url": process.env.DATABASE_URL,
+  },
+  "host": process.env.HOST,
+  "port": parseInt(process.env.PORT) || 3000,
+  "session": process.env.SESSION,
+  "discourseUrl": process.env.DISCOURSE_URL || null,
+  "disqus_shortname": process.env.DISQUS_SHORTNAME || null,
+  "title": process.env.TITLE || "hackdash",
+  "live": process.env.LIVE || true,
+  "mailer": process.env.MAILER || null,
+  "prerender": {
+    "enabled": process.env.PREPENDER_ENABLE || false,
+    "db": process.env.PREPENDER_DB || "mongodb://localhost/prerender"
+  },
+  "team": [
+    "516d1997b2951b02280000e1",
+    "516d1997b2951b02280000e2",
+    "516d1997b2951b02280000e3",
+    "516d1997b2951b02280000e4",
+    "516d1997b2951b02280000e5",
+    "516d1997b2951b02280000e6"
+  ],
+  "maxQueryLimit": process.env.MAX_QUERY_LIMIT || 30,
+  "googleAnalytics": process.env.GOOGLE_ANALYTICS || "UA-XXXXXXXX-X",
+  "facebookAppId": process.env.FACEBOOK_APP_ID || "YYYYYYYYYYYY"
+};
